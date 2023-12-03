a4074c4b3ab9fbd3456d0ca8e75706419c8e2ec5
shintwl <shintwl@icloud.com>
2023-12-03 14:51:44 +0900

    CreateFilter interactor 구현

--- CameraFilterApp/Scenes/CreateFilter/CreateFilterInteractor.swift
+++ CameraFilterApp/Scenes/CreateFilter/CreateFilterInteractor.swift
@@ -9,18 +9,160 @@ import UIKit
 
 protocol CreateFilterBusinessLogic
 {
+    func fetchFilter(request: CreateFilter.FetchFilter.Request)
+    func fetchFilterCategories(request: CreateFilter.FetchFilterCategories.Request)
+    func fetchProperties(request: CreateFilter.FetchProperties.Request)
+    func createFilter(request: CreateFilter.CreateFilter.Request)
+    func editFilter(request: CreateFilter.EditFilter.Request)
+    func deleteFilter(request: CreateFilter.DeleteFilter.Request)
 }
 
 protocol CreateFilterDataStore
 {
-  //var name: String { get set }
+    var filterId: UUID? { get set }
 }
 
 class CreateFilterInteractor: CreateFilterBusinessLogic, CreateFilterDataStore
 {
-  var presenter: CreateFilterPresentationLogic?
-  var worker: CreateFilterWorker?
-  //var name: String = ""
-  
-  // MARK: Do something
+    var presenter: CreateFilterPresentationLogic?
+    var filtersWorker: FiltersWorker = FiltersWorker(filtersStore: FilterMemStore())
+    
+    var filterId: UUID?
+    
+    // MARK: CRUD operations
+    func fetchFilter(request: CreateFilter.FetchFilter.Request) {
+        guard let filterId = self.filterId else { return }
+        
+        filtersWorker.fetchFilter(filterId: filterId) { filter in
+            guard let filter = filter else { return }
+            
+            let response = CreateFilter.FetchFilter.Response(filter: filter)
+        }
+    }
+    
+    func fetchFilterCategories(request: CreateFilter.FetchFilterCategories.Request) {
+        let filterCategories: [CameraFilter.FilterName] = CameraFilter.FilterName.allCases
+        
+        let response = CreateFilter.FetchFilterCategories.Response(filterCategories: filterCategories)
+    }
+    
+    func fetchProperties(request: CreateFilter.FetchProperties.Request) {
+        let filterSystemName = request.filterSystemName
+        
+        var response: CreateFilter.FetchProperties.Response
+        switch filterSystemName {
+        case .CIColorMonochrome:
+            response = CreateFilter.FetchProperties.Response(inputColor: .systemBlue,
+                                                             inputIntensity: (min: 0.0, max: 1.0, value: 1.0),
+                                                             inputRadius: nil,
+                                                             inputLevels: nil)
+        case .CIColorPosterize:
+            response = CreateFilter.FetchProperties.Response(inputColor: nil,
+                                                             inputIntensity: nil,
+                                                             inputRadius: nil,
+                                                             inputLevels: (min: 0.0, max: 10.0, value: 6.0))
+        case .CIBoxBlur:
+            response = CreateFilter.FetchProperties.Response(inputColor: nil,
+                                                             inputIntensity: nil,
+                                                             inputRadius: (min: 0.0, max: 20.0, value: 10.0),
+                                                             inputLevels: nil)
+        default:
+            response = CreateFilter.FetchProperties.Response(inputColor: nil,
+                                                             inputIntensity: nil,
+                                                             inputRadius: nil,
+                                                             inputLevels: nil)
+        }
+    }
+    
+    func createFilter(request: CreateFilter.CreateFilter.Request) {
+        let filterName = request.filterName
+        let filterSystemName = request.filterSystemName
+        let inputColor = request.inputColor
+        let inputIntensity = request.inputIntensity
+        let inputRadius = request.inputRadius
+        let inputLevels = request.inputLevels
+        
+        let filter: CameraFilter? = createFilter(filterId: UUID(),
+                                                 displayName: filterName,
+                                                 systemName: filterSystemName,
+                                                 inputColor: inputColor,
+                                                 inputIntensity: inputIntensity, 
+                                                 inputRadius: inputRadius,
+                                                 inputLevels: inputLevels)
+        
+        guard let filter = filter else { return }
+        
+        filtersWorker.createFilter(filterToCreate: filter) { filter in
+            let response = CreateFilter.CreateFilter.Response(filter: filter)
+        }
+    }
+    
+    func editFilter(request: CreateFilter.EditFilter.Request) {
+        guard let filterId = self.filterId else { return }
+
+        let filterName = request.filterName
+        let filterSystemName = request.filterSystemName
+        let inputColor = request.inputColor
+        let inputIntensity = request.inputIntensity
+        let inputRadius = request.inputRadius
+        let inputLevels = request.inputLevels
+        
+        let filterToUpdate = createFilter(filterId: filterId,
+                                          displayName: filterName,
+                                          systemName: filterSystemName,
+                                          inputColor: inputColor,
+                                          inputIntensity: inputIntensity,
+                                          inputRadius: inputRadius,
+                                          inputLevels: inputLevels)
+        
+        guard let filterToUpdate = filterToUpdate else { return }
+        
+        filtersWorker.updateFilter(filterToUpdate: filterToUpdate) { filter in
+            let response = CreateFilter.EditFilter.Response(filter: filter)
+        }
+    }
+    
+    func deleteFilter(request: CreateFilter.DeleteFilter.Request) {
+        guard let filterId = self.filterId else { return }
+        
+        filtersWorker.deleteFilter(filterId: filterId) { filter in
+            let response = CreateFilter.DeleteFilter.Response(filter: filter)
+        }
+    }
+    
+    //MARK: - private methods
+    private func createFilter(filterId: UUID,
+                              displayName: String,
+                              systemName: CameraFilter.FilterName,
+                              inputColor: UIColor?,
+                              inputIntensity: CGFloat?,
+                              inputRadius: CGFloat?,
+                              inputLevels: CGFloat?) -> CameraFilter? {
+
+        switch systemName {
+        case .CISepiaTone:
+            guard let inputIntensity = inputIntensity else { break }
+            
+            return CameraFilter.createSepiaFilter(filterId: filterId, displayName: displayName, inputIntensity: inputIntensity)
+        case .CIPhotoEffectTransfer:
+            return CameraFilter.createVintageFilter(filterId: filterId, displayName: displayName)
+        case .CIPhotoEffectTonal:
+            return CameraFilter.createBlackWhiteFilter(filterId: filterId, displayName: displayName)
+        case .CIColorMonochrome:
+            guard let inputColor = inputColor,
+                  let inputIntensity = inputIntensity else { break }
+            
+            return CameraFilter.createMonochromeFilter(filterId: filterId, displayName: displayName, inputColor: inputColor.ciColor, inputIntensity: inputIntensity)
+        case .CIColorPosterize:
+            guard let inputLevels = inputLevels else { break }
+            
+            return CameraFilter.createPosterizeFilter(filterId: filterId, displayName: displayName, inputLevels: inputLevels)
+        case .CIBoxBlur:
+            guard let inputRadius = inputRadius else { break }
+            
+            return CameraFilter.createBlurFilter(filterId: filterId, displayName: displayName, inputRadius: inputRadius)
+        }
+        
+        return nil
+    }
 }
