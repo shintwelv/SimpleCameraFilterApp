a6bdb9ff7214ffeb2a5fe5c0d83100c5dfbb846d
shintwl <shintwl@icloud.com>
2023-12-08 15:17:05 +0900

    CreateFilter에서 ListFilter로 돌아가는 라우터 구현

--- CameraFilterApp/Scenes/CreateFilter/CreateFilterRouter.swift
+++ CameraFilterApp/Scenes/CreateFilter/CreateFilterRouter.swift
@@ -9,47 +9,22 @@ import UIKit
 
 @objc protocol CreateFilterRoutingLogic
 {
-  //func routeToSomewhere(segue: UIStoryboardSegue?)
+    func routeToListFilters(segue: UIStoryboardSegue?)
 }
 
 protocol CreateFilterDataPassing
 {
-  var dataStore: CreateFilterDataStore? { get }
+    var dataStore: CreateFilterDataStore? { get }
 }
 
 class CreateFilterRouter: NSObject, CreateFilterRoutingLogic, CreateFilterDataPassing
 {
-  weak var viewController: CreateFilterViewController?
-  var dataStore: CreateFilterDataStore?
-  
-  // MARK: Routing
-  
-  //func routeToSomewhere(segue: UIStoryboardSegue?)
-  //{
-  //  if let segue = segue {
-  //    let destinationVC = segue.destination as! SomewhereViewController
-  //    var destinationDS = destinationVC.router!.dataStore!
-  //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-  //  } else {
-  //    let storyboard = UIStoryboard(name: "Main", bundle: nil)
-  //    let destinationVC = storyboard.instantiateViewController(withIdentifier: "SomewhereViewController") as! SomewhereViewController
-  //    var destinationDS = destinationVC.router!.dataStore!
-  //    passDataToSomewhere(source: dataStore!, destination: &destinationDS)
-  //    navigateToSomewhere(source: viewController!, destination: destinationVC)
-  //  }
-  //}
-
-  // MARK: Navigation
-  
-  //func navigateToSomewhere(source: CreateFilterViewController, destination: SomewhereViewController)
-  //{
-  //  source.show(destination, sender: nil)
-  //}
-  
-  // MARK: Passing data
-  
-  //func passDataToSomewhere(source: CreateFilterDataStore, destination: inout SomewhereDataStore)
-  //{
-  //  destination.name = source.name
-  //}
+    weak var viewController: CreateFilterViewController?
+    var dataStore: CreateFilterDataStore?
+    
+    // MARK: Routing
+    
+    func routeToListFilters(segue: UIStoryboardSegue?) {
+        self.viewController?.dismiss(animated: true)
+    }
 }
