cf24546b3d6b286fab6966dcc5f18a6d67413a61
shintwelv <shintwl@icloud.com>
2023-12-20 12:15:48 +0900

    CameraPreview 유저 모델 정의 추가

--- CameraFilterApp/Scenes/CameraPreview/CameraPreviewModels.swift
+++ CameraFilterApp/Scenes/CameraPreview/CameraPreviewModels.swift
@@ -17,6 +17,53 @@ enum CameraPreview
 {
     // MARK: Use cases
     
+    enum UserAuthError: Equatable, LocalizedError {
+        case cannotSignOut(String)
+        case cannotCheckLogin(String)
+        
+        var errorDescription: String? {
+            switch self {
+            case .cannotSignOut(let string), .cannotCheckLogin(let string):
+                return string
+            }
+        }
+        
+        static func ==(lhs: UserAuthError, rhs: UserAuthError) -> Bool {
+            switch (lhs, rhs) {
+            case (.cannotSignOut(let a), .cannotSignOut(let b)) where a == b: return true
+            case (.cannotCheckLogin(let a), .cannotCheckLogin(let b)) where a == b: return true
+            default: return false
+            }
+        }
+    }
+    
+    enum UserResult<U> {
+        case Success(result: U)
+        case Failure(error: UserAuthError)
+    }
+    
+    enum LoginStatus {
+        struct Request {
+        }
+        struct Response {
+            var signedInUser: UserResult<User?>
+        }
+        struct ViewModel {
+            var signedInUserEmail: String?
+        }
+    }
+    
+    enum SignOut {
+        struct Request {
+        }
+        struct Response {
+            var signedOutUser: UserResult<User>
+        }
+        struct ViewModel {
+            var signedOutUserEmail: String?
+        }
+    }
+    
     struct FilterInfo {
         var filterId: UUID
         var filterName: String
